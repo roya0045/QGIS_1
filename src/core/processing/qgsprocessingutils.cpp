ee45a9aff17373d5cc5114bb4a30967fbd3e4254
roya0045 <roya0045@users.noreply.github.com>
2022-03-11 07:42:55 -0500

    Squashed commit of the following:
    commit 43a96eb11bee1f503bee0a4714736ac96ac52194
    Merge: 12703b55e6 bd6ac0e54d
    Author: Alex <roya0045@users.noreply.github.com>
    Date:   Thu Aug 19 07:57:31 2021 -0400
    
        Merge branch 'master' into autofix-geom
    
    commit 12703b55e6937fe4c2e663a8c1393ff8d3988819
    Author: Alex <roya0045@users.noreply.github.com>
    Date:   Mon Feb 15 13:55:02 2021 -0500
    
        Update qgsvectorlayerfeatureiterator.cpp
    
    commit 42cd26c8995e658a74b7bf62633a2b1c831b1aab
    Author: Alex <roya0045@users.noreply.github.com>
    Date:   Mon Feb 15 12:43:55 2021 -0500
    
        only convert multi to multi

--- src/core/processing/qgsprocessingutils.cpp
+++ src/core/processing/qgsprocessingutils.cpp
@@ -1393,6 +1393,7 @@ QgsProcessingFeatureSource::QgsProcessingFeatureSource( QgsFeatureSource *origin
   , mTransformErrorCallback( context.transformErrorCallback() )
   , mInvalidGeometryCallbackSkip( context.defaultInvalidGeometryCallbackForCheck( QgsFeatureRequest::GeometrySkipInvalid, originalSource ) )
   , mInvalidGeometryCallbackAbort( context.defaultInvalidGeometryCallbackForCheck( QgsFeatureRequest::GeometryAbortOnInvalid, originalSource ) )
+  , mInvalidGeometryCallbackFix( context.defaultInvalidGeometryCallbackForCheck( QgsFeatureRequest::GeometryFixInvalidSkipOnFailure, originalSource ) )
   , mFeatureLimit( featureLimit )
 {}
 
@@ -1535,6 +1536,11 @@ void QgsProcessingFeatureSource::setInvalidGeometryCheck( QgsFeatureRequest::Inv
 
     case QgsFeatureRequest::GeometryAbortOnInvalid:
       mInvalidGeometryCallback = mInvalidGeometryCallbackAbort;
+      break;
+
+    case QgsFeatureRequest::GeometryFixInvalidAbortOnFailure:
+    case QgsFeatureRequest::GeometryFixInvalidSkipOnFailure:
+      mInvalidGeometryCallback = mInvalidGeometryCallbackFix;
       break;
 
   }
