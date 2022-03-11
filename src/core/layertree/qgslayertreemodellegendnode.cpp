a993b0fa731b91af0c81126210edb699a68a018a
roya0045 <roya0045@users.noreply.github.com>
2022-03-11 07:48:22 -0500

    Squashed commit of the following:
    commit 43c382b4b4f68ebe6a3e97aefcd2c47b1c1e3641
    Author: Alex <roya0045@users.noreply.github.com>
    Date:   Mon Apr 19 19:08:12 2021 -0400
    
        space
    
    commit d69c0f5f9dd62967cda6b8fcc4d9c2425f7ceef4
    Author: Alex <roya0045@users.noreply.github.com>
    Date:   Mon Apr 19 17:31:55 2021 -0400
    
        block 3d render eval
    
    commit b8bbdcc54f90586eb80e9bbba5e1b86586b22c81
    Author: Alex <roya0045@users.noreply.github.com>
    Date:   Mon Apr 19 17:28:55 2021 -0400
    
        optimize
    
    commit f277816f6fcd2116bd343f15ad5b1590a239aef9
    Author: Alex <roya0045@users.noreply.github.com>
    Date:   Sun Apr 18 17:56:23 2021 -0400
    
        escape null renderer
    
        Thank you tests
    
    commit 26ce42339a5eab323625b2ba3761541e7f1b2ff6
    Author: Alex <roya0045@users.noreply.github.com>
    Date:   Sun Apr 18 12:26:54 2021 -0400
    
        reduce number of calls
    
    commit 9ff519033647474fcabf95b1e92e9097def98682
    Author: roya0045 <roya0045@users.noreply.github.com>
    Date:   Thu Feb 4 13:22:38 2021 -0500
    
        git rebase plz
    
    commit 00c92db4e32683c4fca022b027b1347bf60ecce9
    Author: roya0045 <roya0045@users.noreply.github.com>
    Date:   Mon Nov 16 08:33:25 2020 -0500
    
        Add variable representing a symbol expression
    
        Fixes #21988

--- src/core/layertree/qgslayertreemodellegendnode.cpp
+++ src/core/layertree/qgslayertreemodellegendnode.cpp
@@ -312,8 +312,11 @@ QgsSymbolLegendNode::QgsSymbolLegendNode( QgsLayerTreeLayer *nodeLayer, const Qg
   }
 
   updateLabel();
+
   if ( QgsVectorLayer *vl = qobject_cast<QgsVectorLayer *>( nodeLayer->layer() ) )
+  {
     connect( vl, &QgsVectorLayer::symbolFeatureCountMapChanged, this, &QgsSymbolLegendNode::updateLabel );
+  }
 
   connect( nodeLayer, &QObject::destroyed, this, [ = ]() { mLayerNode = nullptr; } );
 
@@ -860,25 +863,25 @@ QString QgsSymbolLegendNode::evaluateLabel( const QgsExpressionContext &context,
     QgsExpressionContextScope *symbolScope = createSymbolScope();
     contextCopy.appendScope( symbolScope );
     contextCopy.appendScope( vl->createExpressionContextScope() );
-
+    QString nodeExpression = mLayerNode->labelExpression();
     if ( label.isEmpty() )
     {
       if ( ! mLayerNode->labelExpression().isEmpty() )
-        mLabel = QgsExpression::replaceExpressionText( "[%" + mLayerNode->labelExpression() + "%]", &contextCopy );
+        mLabel = QgsExpression::replaceExpressionText( "[%" + nodeExpression + "%]", &contextCopy );
       else if ( mLabel.contains( "[%" ) )
       {
-        const QString symLabel = symbolLabel();
+        QString symLabel = symbolLabel();
         mLabel = QgsExpression::replaceExpressionText( symLabel, &contextCopy );
       }
       return mLabel;
     }
     else
     {
-      QString eLabel;
+      QString eLabel = QString( label );
       if ( ! mLayerNode->labelExpression().isEmpty() )
-        eLabel = QgsExpression::replaceExpressionText( label + "[%" + mLayerNode->labelExpression() + "%]", &contextCopy );
+        eLabel = QgsExpression::replaceExpressionText( eLabel + "[%" + nodeExpression + "%]", &contextCopy );
       else if ( label.contains( "[%" ) )
-        eLabel = QgsExpression::replaceExpressionText( label, &contextCopy );
+        eLabel = QgsExpression::replaceExpressionText( eLabel, &contextCopy );
       return eLabel;
     }
   }
@@ -895,6 +898,7 @@ QgsExpressionContextScope *QgsSymbolLegendNode::createSymbolScope() const
   if ( vl )
   {
     scope->addVariable( QgsExpressionContextScope::StaticVariable( QStringLiteral( "symbol_count" ), QVariant::fromValue( vl->featureCount( mItem.ruleKey() ) ), true ) );
+    scope->addVariable( QgsExpressionContextScope::StaticVariable( QStringLiteral( "symbol_expression" ), QVariant::fromValue( mLayerNode->symbolExpression( mItem.ruleKey() ) ), true ) );
   }
   return scope;
 }
