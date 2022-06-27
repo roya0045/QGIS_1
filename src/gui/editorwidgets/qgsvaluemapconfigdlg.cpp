bc8d7c6e3bb795917915573f1028383544079216
Alex <roya0045@users.noreply.github.com>
2022-06-27 09:11:34 -0400

    Squashed commit of the following:
    commit aea9e22d6d47874d75a0ea9da3674135f4b56ae5
    Author: roya0045 <roya0045@users.noreply.github.com>
    Date:   Thu Jun 23 15:58:25 2022 -0400
    
        v1
    
    commit 57751067f1d8f85cd118251c3ca72563661daf79
    Author: roya0045 <roya0045@users.noreply.github.com>
    Date:   Thu Jun 23 12:01:52 2022 -0400
    
        get prec

--- src/gui/editorwidgets/qgsvaluemapconfigdlg.cpp
+++ src/gui/editorwidgets/qgsvaluemapconfigdlg.cpp
@@ -29,7 +29,8 @@
 #include <QRegularExpression>
 
 QgsValueMapConfigDlg::QgsValueMapConfigDlg( QgsVectorLayer *vl, int fieldIdx, QWidget *parent )
-  : QgsEditorConfigWidget( vl, fieldIdx, parent )
+  : QgsEditorConfigWidget( vl, fieldIdx, parent ),
+  mField( vl->fields().at( fieldIdx ) )
 {
   setupUi( this );
 
@@ -49,6 +50,8 @@ QgsValueMapConfigDlg::QgsValueMapConfigDlg( QgsVectorLayer *vl, int fieldIdx, QW
 QVariantMap QgsValueMapConfigDlg::config()
 {
   QList<QVariant> valueList;
+
+  validateKeys();
 
   //store data to map
   for ( int i = 0; i < tableWidget->rowCount() - 1; i++ )
@@ -110,6 +113,7 @@ void QgsValueMapConfigDlg::setConfig( const QVariantMap &config )
         setRow( row, mit.value().toString(), mit.key() );
     }
   }
+  validateKeys();
 }
 
 void QgsValueMapConfigDlg::vCellChanged( int row, int column )
@@ -183,6 +187,7 @@ void QgsValueMapConfigDlg::updateMap( const QList<QPair<QString, QVariant>> &lis
       setRow( row, pair.first, pair.second.toString() );
     ++row;
   }
+  validateKeys();
 }
 
 void QgsValueMapConfigDlg::populateComboBox( QComboBox *comboBox, const QVariantMap &config, bool skipNull )
@@ -212,6 +217,7 @@ void QgsValueMapConfigDlg::populateComboBox( QComboBox *comboBox, const QVariant
       comboBox->addItem( it.key(), it.value() );
     }
   }
+  validateKeys();
 }
 
 bool QgsValueMapConfigDlg::eventFilter( QObject *watched, QEvent *event )
@@ -251,6 +257,7 @@ void QgsValueMapConfigDlg::setRow( int row, const QString &value, const QString 
   }
   tableWidget->setItem( row, 0, valueCell );
   tableWidget->setItem( row, 1, descriptionCell );
+  validateKeys();
 }
 
 void QgsValueMapConfigDlg::copySelectionToClipboard()
@@ -344,4 +351,33 @@ void QgsValueMapConfigDlg::loadMapFromCSV( const QString &filePath )
   }
 
   updateMap( map, false );
+}
+
+bool QgsValueMapConfigDlg::validateKeys()
+{
+  QString *errorMessage;
+  bool allValid = true;
+  for ( int i = 0; i < tableWidget->rowCount() - 1; i++ )
+  {
+    QTableWidgetItem *ki = tableWidget->item( i, 0 );
+    QTableWidgetItem *vi = tableWidget->item( i, 1 );
+
+    if ( !ki )
+      continue;
+
+    QString ks = ki->text();
+    if ( ! mField.convertCompatible( ks, errorMessage ) )
+    {
+      QMessageBox::information( nullptr,
+                          tr( "Map/Value config" ),
+                          tr( "%1" ).arg( errorMessage ),
+                          QMessageBox::Cancel );
+      ki->setBackground( QBrush( Qt::red ) );
+      allValid = false;
+    }
+    else
+      ki->setBackground( vi->background() );
+
+  }
+  return allValid;
 }
