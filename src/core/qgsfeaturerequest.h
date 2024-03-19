95ce54f0593dfbfdecfd38008b6f4ae4cab52598
roya0045 <roya0045@users.noreply.github.com>
2024-03-19 09:05:33 -0400

    Update qgsfeaturerequest.h

--- src/core/qgsfeaturerequest.h
+++ src/core/qgsfeaturerequest.h
@@ -935,7 +935,6 @@ class CORE_EXPORT QgsFeatureRequest
     QgsCoordinateReferenceSystem mCrs;
     QgsCoordinateTransformContext mTransformContext;
     int mTimeout = -1;
-    AutoFixIntensity mAutofixFlag = FixAtAllCosts;
     int mRequestMayBeNested = false;
     QgsFeedback *mFeedback = nullptr;
 };
