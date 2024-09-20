8ab9f91e8f6b9c7f5e5e920f15912dbe21664506
Alex <roya0045@users.noreply.github.com>
2024-09-20 09:22:47 -0400

    do the loop

--- src/gui/layertree/qgslayertreeview.cpp
+++ src/gui/layertree/qgslayertreeview.cpp
@@ -685,8 +685,7 @@ void QgsLayerTreeView::keyPressEvent( QKeyEvent *event )
         {
           legendNode->setVisibility( ! isFirstNodeChecked ); // TO IMPLEMENT
         }
-      // if we call the original keyPress handler, the current item will be checked to the original state yet again
-      return;
+      }
     }
   }
 
